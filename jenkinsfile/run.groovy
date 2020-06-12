b2cbb7bebdc173ecd51ae5ad0ee549a9fa6138c1
Ivan <sit7602@gmail.com>
2020-06-12 19:38:15 +0300

    fix run file

--- jenkinsfile/run.groovy
+++ jenkinsfile/run.groovy
@@ -2,14 +2,14 @@
 pipeline {
     agent{node('master')}
     stages {
-        stage('Dowload project') {
+        stage('Clean workspace & dowload dist') {
             steps {
                 script {
                     cleanWs()
                     //git(branch: 'master', credentialsId: 'IvanSitnikovGit', url: 'https://github.com/sitozzz/jenkins_education.git')
                 }
                 script {
-                    echo 'Start download project'
+                    echo 'Update from repository'
                     checkout([$class                           : 'GitSCM',
                               branches                         : [[name: '*/master']],
                               doGenerateSubmoduleConfigurations: false,
@@ -20,11 +20,11 @@ pipeline {
                 }
             }
         }
-        stage ('Create docker image'){
+        stage ('Build & run docker image'){
             steps{
                 script{
-                    sh "docker build ${WORKSPACE}/auto -t webapp"
-                    sh "docker run -d webapp"
+                    sh "docker build ${WORKSPACE}/auto -t ivan_sitnikov_nginx"
+                    sh "docker run -d ivan_sitnikov_nginx"
                 }
             }
         }
