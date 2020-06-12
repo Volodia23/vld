0bf85ba8299eed728ee48040f394e4bf6b8c9298
Ivan <sit7602@gmail.com>
2020-06-12 19:29:03 +0300

    add dockerfile & test html page

--- jenkinsfile/run.groovy
+++ jenkinsfile/run.groovy
@@ -2,10 +2,11 @@
 pipeline {
     agent{node('master')}
     stages {
-        stage('Clone project') {
+        stage('Dowload project') {
             steps {
                 script {
                     cleanWs()
+                    //git(branch: 'master', credentialsId: 'IvanSitnikovGit', url: 'https://github.com/sitozzz/jenkins_education.git')
                 }
                 script {
                     echo 'Start download project'
@@ -19,15 +20,16 @@ pipeline {
                 }
             }
         }
-        stage ('Build docker image'){
+        stage ('Create docker image'){
             steps{
                 script{
                     sh "docker build ${WORKSPACE}/auto -t webapp"
                     sh "docker run -d webapp"
-                    sh "docker exec -it webapp "df -h > ~/proc""
                 }
             }
         }
         
     }
+
+    
 }
